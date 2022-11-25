e1a70076afe138b3ffcb91d42aebd3e60c289dc6
wehigami <erykkontaktoff@gmail.com>
2022-11-25 18:25:33 +0100

    fixed background video

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -10,7 +10,7 @@ function MainPage() {
         <div className="bg-zinc-900 text-zinc-200">
             <Header />
             {/**each one of these props needs to be a list */}
-            <Section divItems={[titles, [imgs[0], 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quas omnis possimus nam, commodi nulla provident vero dicta mollitia iure earum quam animi! Corrupti'], ['test', 'test'], ['test']]}/>
+            <Section divItems={[titles, [imgs[0], 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quas omnis possimus nam, commodi nulla provident vero dicta mollitia iure earum quam animi! Corrupti'], ['test', 'test']]}/>
         </div>
     )
 }
