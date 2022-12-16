0dedd00c65be674ad7dda30135b826352122703c
Wehi <notstrangequark@gmail.com>
2022-12-16 01:04:38 +0100

    added some functionality to notes

--- src/components/notes/section/noteSection.jsx
+++ src/components/notes/section/noteSection.jsx
@@ -1,10 +1,12 @@
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./noteSection.scss";
 
+
+
 const boxVariant = {
-  visible: { opacity: 1, transition: { duration: 0.3 }, x: 0 },
+  visible: { opacity: 1, x: 0 },
   hidden: { opacity: 0, x: -50 },
 };
 
@@ -18,11 +20,10 @@ const xVariant = {
   hidden: { opacity: 0, y: 50 },
 };
 
-const categories = ["All"];
-
 function NoteSection() {
   const control = useAnimation();
   const [ref, inView] = useInView();
+
 
   useEffect(() => {
     if (inView) {
@@ -30,26 +31,58 @@ function NoteSection() {
     }
   }, [control, inView]);
 
+
+  const categories = [
+    {
+      name: "All",
+      notes: [
+        {
+          name: "tester",
+          contents: "lorem",
+          title: 'ipsusum'
+        },
+      ],
+      active: true,
+    },
+    {
+      name: "test",
+      notes: [
+        {
+          name: "test1",
+          contents: "ipsum",
+          title: 'none'
+        },
+      ],
+      active: false,
+    },
+  ];
+
   return (
     <div className="grid grid-cols-5 bg-zinc-900 h-screen overflow-hidden">
-      <div className="border-r border-zinc-700 col-span-1 p-4 grid grid-rows-10">
+      <div className="grid-section col-span-1 p-4 grid grid-rows-10">
         <div className="text-2xl row-span-6">
-          <motion.p
-            variants={boxVariant}
-            ref={ref}
-            initial="hidden"
-            animate={control}
-            className="mt-4"
-          >
-            test
-          </motion.p>
+          {categories.map((category) => (
+            <p key={category}>
+              <motion.button
+                variants={boxVariant}
+                ref={ref}
+                initial="hidden"
+                animate={control}
+                className="mt-4"
+                onClick={() => category.active === !category.active}
+              >
+                {category.name}
+              </motion.button>
+            </p>
+          ))}
         </div>
+
         <button className="border border-zinc-600 rounded-full px-4 py-2 text-2xl w-14 h-14 place-self-end">
           +
         </button>
       </div>
 
-      <div className="col-span-2 border-r border-zinc-700 p-2">
+      <div className="col-span-2 grid-section p-2">
         <motion.h1
           variants={testVariant}
           ref={ref}
@@ -59,6 +92,17 @@ function NoteSection() {
         >
           My notes
         </motion.h1>
+        {categories.map((category) =>
+          category.notes.map((note) =>
+            category.active ? (
+              <p key={category.name} className="my-4">
+                <button>{note.name}</button>
+              </p>
+            ) : (
+              <></>
+            )
+          )
+        )}
       </div>
 
       <div className="col-span-2 grid place-items-center p-2">
