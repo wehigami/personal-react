70181939df22329383ecbf6d5ec11ddc5656325b
Wehi <notstrangequark@gmail.com>
2023-01-12 17:27:52 +0100

    working google login

--- src/components/nav/loginNav/loginPopup.jsx
+++ src/components/nav/loginNav/loginPopup.jsx
@@ -1,11 +1,35 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useRef, useEffect } from "react";
 import { activateLogin } from "../../../redux/loginActiveSlice";
+import { GoogleLogin } from "react-google-login";
+import { gapi } from "gapi-script";
+import GoogleButton from "react-google-button";
 import "./popup.scss";
+
+const clientId = "569807940466-7qk36mvs2m55biihg3c4ief5orn6tube.apps.googleusercontent.com";
+
 
 function LoginPopup() {
   const { active } = useSelector((state) => state.loginActive);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const initClient = () => {
+      gapi.client.init({
+        clientId:
+          "569807940466-7qk36mvs2m55biihg3c4ief5orn6tube.apps.googleusercontent.com",
+        scope: "",
+      });
+    };
+    gapi.load("client:auth2", initClient);
+  });
+
+  const onSuccess = (res) => {
+    console.log("success:", res);
+  };
+  const onFailure = (err) => {
+    console.log("failed:", err);
+  };
 
   function useOutsideAlerter(ref) {
     useEffect(() => {
@@ -34,7 +58,23 @@ function LoginPopup() {
       className="fixed w-96 h-96 z-40 text-center mb-96 main"
       ref={wrapperRef}
     >
-      <h1 className="text-xl">test</h1>
+      <h1 className="text-xl mt-6">Log in</h1>
+      <div className="googleLogin">
+      <p className="border-b w-64 pb-2 text-center mt-16 mb-24 border-zinc-600">For now:</p>
+      </div>
+      <div className="googleLogin">
+        <GoogleLogin
+          clientId={clientId}
+          render={(renderProps) => (
+            <GoogleButton onClick={renderProps.onClick} type="light">
+              Sign in with Google
+            </GoogleButton>
+          )}
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          cookiePolicy={"single_host_origin"}
+        />
+      </div>
     </div>
   ) : (
     <></>
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import Notes from './pages/notes-app';
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
